86c9ebaaad90bca5fed0f13f259ceefe70e4909f
Dimitrij Drus <dimitrij.drus@innoq.com>
2020-01-16 13:16:03 +0100

    creating OCSP responder key and certificate during Root CA setup

--- setup_root_ca.sh
+++ setup_root_ca.sh
@@ -2,28 +2,52 @@
 
 CA_DIR=./root_ca
 CA_CONFIG=./rootca.conf
-PRIVATE_KEY_FILE=$CA_DIR/private/root_ca_key.pem
-CERTIFICATE_FILE=$CA_DIR/certs/root_ca_cert.pem
+CSR_CONFIG=./csr.conf
+CA_PRIVATE_KEY_FILE=$CA_DIR/private/root_ca_key.pem
+CA_CERTIFICATE_FILE=$CA_DIR/certs/root_ca_cert.pem
+OCSP_PRIVATE_KEY_FILE=$CA_DIR/private/root_ca_ocsp_key.pem
+OCSP_CSR_FILE=$CA_DIR/csr/root_ca_ocsp_csr.pem
+OCSP_CERTIFICATE_FILE=$CA_DIR/certs/root_ca_ocsp_cert.pem
 
 # create directory structure required
 mkdir -p \
     $CA_DIR/certs \
     $CA_DIR/crl \
     $CA_DIR/newcerts \
-    $CA_DIR/private
+    $CA_DIR/private \
+    $CA_DIR/csr
     
 # create files required
 touch $CA_DIR/index.txt
 echo 1000 > $CA_DIR/serial
 
 # generate Root CA key
-openssl genrsa -out $PRIVATE_KEY_FILE 4096
-chmod 400 $PRIVATE_KEY_FILE
+openssl genrsa -out $CA_PRIVATE_KEY_FILE 4096
+chmod 400 $CA_PRIVATE_KEY_FILE
 
 # create Root CA certificate
 openssl req -config $CA_CONFIG \
-            -key $PRIVATE_KEY_FILE \
+            -key $CA_PRIVATE_KEY_FILE \
             -new -x509 -days 3650 -extensions root_ca \
-            -out $CERTIFICATE_FILE
+            -out $CA_CERTIFICATE_FILE
             
-chmod 444 $CERTIFICATE_FILE
+chmod 444 $CA_CERTIFICATE_FILE
+
+echo "Generating private key for the OCSP responder"
+openssl genrsa -out $OCSP_PRIVATE_KEY_FILE 2048
+chmod 400 $OCSP_PRIVATE_KEY_FILE
+
+echo "Creating CSR to request the OCSP responder certificate from the Issuing CA"
+openssl req -config $CSR_CONFIG \
+            -new \
+            -key $OCSP_PRIVATE_KEY_FILE \
+            -out $OCSP_CSR_FILE
+
+echo "Sing the OCSP certificate by the Issuing CA"
+openssl ca -config $CA_CONF \
+           -extensions ocsp \
+           -days 365 -notext \
+           -in $OCSP_CSR_FILE \
+           -out $OCSP_CERTIFICATE_FILE 
+
+chmod 444 $OCSP_CERTIFICATE_FILE
