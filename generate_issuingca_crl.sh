8e4a953b06444ea893aad8d4ea38b9828246fa57
Dimitrij Drus <dimitrij.drus@innoq.com>
2020-01-16 15:45:46 +0100

    added creation of bundled CRLs in one file (containing Root CA CRL and Issuing CA CRL)

--- generate_issuingca_crl.sh
+++ generate_issuingca_crl.sh
@@ -7,3 +7,10 @@ openssl ca -config issuingca.conf \
 openssl crl -in issuing_ca/crl/crl.pem \
             -out issuing_ca/crl/crl.der \
             -outform der
+
+# create a combined CRL file containing the Root CA CRL and Issuing CA CRL
+
+if [ -s root_ca/crl/crl.pem ]; then
+  cat root_ca/crl/crl.pem > issuing_ca/crl/combined_crl.pem
+  cat issuing_ca/crl/crl.pem >> issuing_ca/crl/combined_crl.pem
+fi
