c45401ff7363fa6696acbe009927260135232518
Dimitrij Drus <dimitrij.drus@innoq.com>
2020-01-27 13:39:36 +0100

    ocsp script fixed

--- ocsp/ocsp.sh
+++ ocsp/ocsp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-openssl ocsp -port 2552 -text -index /opt/ocsp/index.txt -CA /opt/ocsp/ca-chain.pem -rkey /opt/ocsp/key.pem -rsigner /opt/ocsp/cert.pem
+openssl ocsp -port 2552 -text -index /opt/ocsp/index.txt -CA /opt/ocsp/ca_cert.pem -rkey /opt/ocsp/key.pem -rsigner /opt/ocsp/cert.pem
